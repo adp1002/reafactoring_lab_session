9d5a379b1b7dff982118e2eb1fa0999cfdec1360
adp1002 <adp1002@alu.ubu.es>
2020-05-09 20:49:19 +0200

    Crear subclases de Node

---
+++ src/lanSimulation/internals/Printer.java
@@ -0,0 +1,9 @@
+package lanSimulation.internals;
+
+public class Printer extends Node {
+
+	public Printer() {
+		super(Node.PRINTER, "Printer");
+	}
+
+}
---
+++ src/lanSimulation/internals/WorkStation.java
@@ -0,0 +1,9 @@
+package lanSimulation.internals;
+
+public class WorkStation extends Node {
+
+	public WorkStation() {
+		super(Node.WORKSTATION, "WorkStation");
+	}
+
+}
